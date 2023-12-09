573a5e455cdb987aac56764bb3ef0007546582e6
kartikjn <kartikjain42@protonmail.com>
2023-12-09 14:44:56 +0530

    Initialize subgraph

---
+++ ez-pay/tests/ez-pay-utils.ts
@@ -0,0 +1,177 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, Bytes, BigInt } from "@graphprotocol/graph-ts"
+import {
+  ChangeRequested,
+  ClaimTokens,
+  CollateralWithdrawn,
+  EMIPaid,
+  Liquidated,
+  LoanRequested,
+  LoanTransferred,
+  RequestAccepted
+} from "../generated/EzPay/EzPay"
+
+export function createChangeRequestedEvent(
+  lender: Address,
+  borrower: Address,
+  id: Bytes,
+  notificationToLender: boolean
+): ChangeRequested {
+  let changeRequestedEvent = changetype<ChangeRequested>(newMockEvent())
+
+  changeRequestedEvent.parameters = new Array()
+
+  changeRequestedEvent.parameters.push(
+    new ethereum.EventParam("lender", ethereum.Value.fromAddress(lender))
+  )
+  changeRequestedEvent.parameters.push(
+    new ethereum.EventParam("borrower", ethereum.Value.fromAddress(borrower))
+  )
+  changeRequestedEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromFixedBytes(id))
+  )
+  changeRequestedEvent.parameters.push(
+    new ethereum.EventParam(
+      "notificationToLender",
+      ethereum.Value.fromBoolean(notificationToLender)
+    )
+  )
+
+  return changeRequestedEvent
+}
+
+export function createClaimTokensEvent(id: Bytes): ClaimTokens {
+  let claimTokensEvent = changetype<ClaimTokens>(newMockEvent())
+
+  claimTokensEvent.parameters = new Array()
+
+  claimTokensEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromFixedBytes(id))
+  )
+
+  return claimTokensEvent
+}
+
+export function createCollateralWithdrawnEvent(
+  borrower: Address,
+  token: Address,
+  amount: BigInt
+): CollateralWithdrawn {
+  let collateralWithdrawnEvent = changetype<CollateralWithdrawn>(newMockEvent())
+
+  collateralWithdrawnEvent.parameters = new Array()
+
+  collateralWithdrawnEvent.parameters.push(
+    new ethereum.EventParam("borrower", ethereum.Value.fromAddress(borrower))
+  )
+  collateralWithdrawnEvent.parameters.push(
+    new ethereum.EventParam("token", ethereum.Value.fromAddress(token))
+  )
+  collateralWithdrawnEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+
+  return collateralWithdrawnEvent
+}
+
+export function createEMIPaidEvent(borrower: Address, amount: BigInt): EMIPaid {
+  let emiPaidEvent = changetype<EMIPaid>(newMockEvent())
+
+  emiPaidEvent.parameters = new Array()
+
+  emiPaidEvent.parameters.push(
+    new ethereum.EventParam("borrower", ethereum.Value.fromAddress(borrower))
+  )
+  emiPaidEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+
+  return emiPaidEvent
+}
+
+export function createLiquidatedEvent(
+  lender: Address,
+  borrower: Address,
+  amount: BigInt
+): Liquidated {
+  let liquidatedEvent = changetype<Liquidated>(newMockEvent())
+
+  liquidatedEvent.parameters = new Array()
+
+  liquidatedEvent.parameters.push(
+    new ethereum.EventParam("lender", ethereum.Value.fromAddress(lender))
+  )
+  liquidatedEvent.parameters.push(
+    new ethereum.EventParam("borrower", ethereum.Value.fromAddress(borrower))
+  )
+  liquidatedEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+
+  return liquidatedEvent
+}
+
+export function createLoanRequestedEvent(
+  paymentToken: Address,
+  borrower: boolean,
+  id: Bytes
+): LoanRequested {
+  let loanRequestedEvent = changetype<LoanRequested>(newMockEvent())
+
+  loanRequestedEvent.parameters = new Array()
+
+  loanRequestedEvent.parameters.push(
+    new ethereum.EventParam(
+      "paymentToken",
+      ethereum.Value.fromAddress(paymentToken)
+    )
+  )
+  loanRequestedEvent.parameters.push(
+    new ethereum.EventParam("borrower", ethereum.Value.fromBoolean(borrower))
+  )
+  loanRequestedEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromFixedBytes(id))
+  )
+
+  return loanRequestedEvent
+}
+
+export function createLoanTransferredEvent(
+  lender: Address,
+  borrower: Address,
+  id: Bytes
+): LoanTransferred {
+  let loanTransferredEvent = changetype<LoanTransferred>(newMockEvent())
+
+  loanTransferredEvent.parameters = new Array()
+
+  loanTransferredEvent.parameters.push(
+    new ethereum.EventParam("lender", ethereum.Value.fromAddress(lender))
+  )
+  loanTransferredEvent.parameters.push(
+    new ethereum.EventParam("borrower", ethereum.Value.fromAddress(borrower))
+  )
+  loanTransferredEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromFixedBytes(id))
+  )
+
+  return loanTransferredEvent
+}
+
+export function createRequestAcceptedEvent(
+  id: Bytes,
+  user: Address
+): RequestAccepted {
+  let requestAcceptedEvent = changetype<RequestAccepted>(newMockEvent())
+
+  requestAcceptedEvent.parameters = new Array()
+
+  requestAcceptedEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromFixedBytes(id))
+  )
+  requestAcceptedEvent.parameters.push(
+    new ethereum.EventParam("user", ethereum.Value.fromAddress(user))
+  )
+
+  return requestAcceptedEvent
+}
---
+++ ez-pay/tests/ez-pay.test.ts
@@ -0,0 +1,70 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address, Bytes, BigInt } from "@graphprotocol/graph-ts"
+import { ExampleEntity } from "../generated/schema"
+import { ChangeRequested } from "../generated/EzPay/EzPay"
+import { handleChangeRequested } from "../src/ez-pay"
+import { createChangeRequestedEvent } from "./ez-pay-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let lender = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let borrower = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let id = Bytes.fromI32(1234567890)
+    let notificationToLender = "boolean Not implemented"
+    let newChangeRequestedEvent = createChangeRequestedEvent(
+      lender,
+      borrower,
+      id,
+      notificationToLender
+    )
+    handleChangeRequested(newChangeRequestedEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("ExampleEntity created and stored", () => {
+    assert.entityCount("ExampleEntity", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "lender",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "borrower",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "notificationToLender",
+      "boolean Not implemented"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
